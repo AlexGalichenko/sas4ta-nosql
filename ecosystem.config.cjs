cb7a0697fe2350922f6c3199c01ca7b82057db71
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-03-26 20:35:51 +0300

    added pm2 clustering

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+        {
+            name: "chess",
+            script: "./app.js",
+            instances: 3,
+            exec_mode : "cluster",
+            env: {
+                "NODE_ENV": "development",
+                "MONO_URI": process.env.MONGO_URI,
+                "X_API_KEY": process.env.X_API_KEY
+            }
+        }
+    ]
+}
